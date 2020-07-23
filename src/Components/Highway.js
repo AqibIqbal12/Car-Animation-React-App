97cef1d77a957b7ae9a419a032a2322a27ea230f
Aqib_Iqbal <aqibiqbal48@gmail.com>
2020-07-23 22:32:30 +0500

    Create Highway.js

---
+++ src/Components/Highway.js
@@ -0,0 +1,31 @@
+import React,{useEffect} from 'react';
+import useWebAnimations from "@wellyshen/use-web-animations";
+
+
+export const Highway = () => {
+
+    const  {ref, getAnimation} = useWebAnimations({
+        keyframes: [
+          { transform: 'translateX(-3400px)' } 
+        ],
+        timing: {
+          duration: 50000,
+          iterations: Infinity
+        },
+      });
+
+      useEffect(() => {
+        document.addEventListener("click", (e) => {
+          const animation = getAnimation();
+         animation.updatePlaybackRate(animation.playbackRate *= 1.1);
+        });
+      }, [getAnimation]);
+    
+    return (
+
+        <div className="highway" ref={ref}>
+
+        </div>
+
+    )
+}
