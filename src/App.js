9110a5b01cf2179e33228b301b6bfe79c268d8b2
Aqib_Iqbal <aqibiqbal48@gmail.com>
2020-07-23 22:32:00 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Highway} from './Components/Highway';
+import {Clouds} from './Components/Clouds';
+import {City} from './Components/City';
+import {Car} from './Components/Car';
+import {Backwheel} from './Components/Backwheel';
+import {Frontwheel} from './Components/Frontwheel'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
 
-export default App;
+    return (
+
+    <div className="main">
+      <Highway/>
+      <Clouds/>
+      <City/>
+      <Car/>
+      <Backwheel/>
+      <Frontwheel/>
+
+    </div>
+  )
+}
